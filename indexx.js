852f5c658af5462a7c00f2863d3a93fe3fda20ca
Prabhath Haridas <prabhathharidas2020@gmail.com>
2023-08-05 20:23:05 +0530

    first commit~

---
+++ indexx.js
@@ -0,0 +1,32 @@
+//let a = " Hello World "
+//a = 90;       /* change variables - let*/
+
+//var a = 90; /*variables declaring - var*/
+
+//console.log(a+b); /* Output code - console.log*/
+
+// Data Types :
+
+//string
+ let str = " hello world ";
+
+ //number
+ let num = 90;
+
+ //big int 
+ let age = 5465645465545645;
+
+ //boolean
+ let isChecked = false;
+
+ //undefined - not giving value
+ let abc; 
+
+ //null - not know the value
+ let xyz = null
+
+ //array
+ let users = [4,5,6,6,7,"hello"];
+ console.log(users);
+
+ let user= {first_name = "hello",3,4,};
